b244c7e326ba2dd182501aa4e11de3fe8140e05f
Eliezer Valenzuela <vzla0094@gmail.com>
2020-11-25 11:42:17 -0700

    added styled components; added and configured  enzyme dependencies; created tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-comment-textnodes */
+import "./App.css"
+import styled from "styled-components"
+import { useState } from "react"
+
+export const AppContainer = styled.div`
+  text-align: center;
+`
+
+export const Background = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+export const Button = styled.button`
+  background-color: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+export const Result = styled.div`
+  background-color: black;
+  padding: 0.25em 3em;
+`
 
 function App() {
+  const [numbers, setNumbers] = useState({
+    firstNumber: "",
+    secondNumber: "",
+  })
+
+  const [result, setResult] = useState("")
+
+  const addNumbers = (a, b) => {
+    const aInt = parseInt(a)
+    const bInt = parseInt(b)
+    setResult(aInt + bInt)
+  }
+
+  const changeNumbers = (number, value) => {
+    setNumbers((prevState) => ({
+      ...prevState,
+      [number]: value,
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <AppContainer>
+      <Background>
+        <input
+          name="firstNumber"
+          type="number"
+          placeholder="enter a number"
+          value={numbers.firstNumber}
+          onChange={(event) =>
+            changeNumbers(event.target.name, event.target.value)
+          }
+        ></input>
+        <input
+          name="secondNumber"
+          type="number"
+          placeholder="enter a number"
+          value={numbers.secondNumber}
+          onChange={(event) =>
+            changeNumbers(event.target.name, event.target.value)
+          }
+        ></input>
+        <Button
+          onClick={() => addNumbers(numbers.firstNumber, numbers.secondNumber)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add the numbers
+        </Button>
+        <Result>
+          <h4>Result:</h4>
+          <p id="result">{result}</p>
+        </Result>
+      </Background>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App, { Button } from "./App"
+import { shallow, mount } from "enzyme"
+import toJson from "enzyme-to-json"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it("should render App without crashing", () => {
+  shallow(<App />)
+})
+
+let wrapper
+
+beforeAll(() => {
+  wrapper = shallow(<App></App>)
+  const firstNumber = wrapper.find("input[name='firstNumber']")
+  const secondNumber = wrapper.find("input[name='secondNumber']")
+  firstNumber.simulate("change", {
+    target: { name: "firstNumber", value: 5 },
+  })
+  secondNumber.simulate("change", {
+    target: { name: "secondNumber", value: 10 },
+  })
+})
+
+it("should add first and second numbers", () => {
+  // ALWAYS RE-FIND after anything changes (simulate)!
+  // https://github.com/enzymejs/enzyme/issues/1757#issuecomment-416669094
+  const firstNumber = wrapper.find("input[name='firstNumber']")
+  const secondNumber = wrapper.find("input[name='secondNumber']")
+  const firstNumberValue = firstNumber.props().value
+  const secondNumberValue = secondNumber.props().value
+
+  const addButton = wrapper.find(Button)
+  addButton.simulate("click")
+  const result = parseInt(wrapper.find("#result").props().children)
+
+  expect(result).toEqual(firstNumberValue + secondNumberValue)
+})
+
+//Testing styled components
+// https://github.com/styled-components/styled-components/issues/896
