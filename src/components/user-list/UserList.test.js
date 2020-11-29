5cbc93519fbf1c49ca1eca54b6eb3765d9353d9c
Eliezer Valenzuela <vzla0094@gmail.com>
2020-11-29 12:56:42 -0700

    refactored code eliminating getAsyncTestCallback and instead updating the wrapper inside a beforeAll function

--- src/components/user-list/UserList.test.js
+++ src/components/user-list/UserList.test.js
@@ -5,13 +5,6 @@ import { mocks } from "./UserList.mocks"
 import { MockedProvider } from "@apollo/client/testing"
 import { waitFor } from "@testing-library/react"
 
-const getAsyncTestCallback = (wrapper, testCallbackBody) => async () => {
-  await waitFor(() => {
-    wrapper.update()
-    testCallbackBody()
-  })
-}
-
 describe("<UserList/>", () => {
   const UserListWrapped = (
     <MockedProvider mocks={mocks} addTypename={false}>
@@ -21,17 +14,18 @@ describe("<UserList/>", () => {
 
   const wrapper = mount(UserListWrapped)
 
-  it(
-    "should render a title <h2 class='title'>Users:</h2>",
-    getAsyncTestCallback(wrapper, () => {
-      expect(wrapper.find("h2[className='title']").text()).toContain("Users:")
+  beforeAll(async () => {
+    await waitFor(() => {
+      wrapper.update()
+      expect(wrapper.exists(".loading")).toBeFalsy()
     })
-  )
+  })
 
-  it(
-    "should render 10 names inside <p class='user'/> elements",
-    getAsyncTestCallback(wrapper, () => {
-      expect(wrapper.find("p[className='user']")).toHaveLength(10)
-    })
-  )
+  it("should render a title <h2 class='title'>Users:</h2>", () => {
+    expect(wrapper.find("h2[className='title']").text()).toContain("Users:")
+  })
+
+  it("should render 10 names inside <p class='user'/> elements", () => {
+    expect(wrapper.find("p[className='user']")).toHaveLength(10)
+  })
 })
